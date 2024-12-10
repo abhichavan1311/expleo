64ba2dc546cd5dc614f6994423ce869f26d34d9e
abhishek.chavan <abhichavan1311@gmail.com>
2024-12-10 14:12:14 +0530

    updated

--- AWS_automation_using_boto3/7.calling_one_module_into_other_code.py
+++ AWS_automation_using_boto3/7.calling_one_module_into_other_code.py
@@ -1,10 +1,10 @@
-refer dir 7.calling_one_module_into_other_code
+refer dir AWS_automation_using_boto3/Importing_one_python_module_into_another/
 
-2 tasks are mentioned here
-
+2 tasks are mentioned here - 
 simple task: main_script1.py
 and 
 Complex task: main_script2.py
 
+Note: sns_utils.py is module which we have used in both above tasks/scripts
 
 for example output of main_script2.py refer AWS_automation_using_boto3/readme.md
